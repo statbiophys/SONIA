62c828cb02eb0717959df2eebcebe70867331888
camacesco <62846799+camacesco@users.noreply.github.com>
2021-02-09 15:14:23 +0100

    Update sonia.py
    dtype object for features array

--- sonia/sonia.py
+++ sonia/sonia.py
@@ -515,7 +515,7 @@ class Sonia(object):
 
         if len(add_features) > 0:
             if len(self.features) == 0:
-                self.features = np.array(add_features)
+                self.features = np.array(add_features, dtype=object)
             else:
                 self.features = np.append(self.features, add_features, axis = 0)
             self.update_model_structure(initialize=True)
